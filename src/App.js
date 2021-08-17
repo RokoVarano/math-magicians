0e7388fad5dc70369611aa3e9b120588a341778a
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-08-17 18:01:30 -0400

    Add Home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Header from './components/Header';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <div className="App">
           <Header />
           <Switch>
+            <Route path="/" exact component={Home} />
             <Route path="/calculator" component={Calculator} />
           </Switch>
         </div>
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  const text = `Cat ipsum dolor sit amet, chew the plant or murf pratt ungow ungow yet pet my belly, you know you want to; seize the hand and shred it! and small kitty warm kitty little balls of fur. Stick butt in face hide at bottom of staircase to trip human. Ignore the human until she needs to get up, then climb on her lap and sprawl eat all the power cords fat baby cat best buddy little guy lie in the sink all day, milk the cow. Who's the baby under the bed eat grass, throw it back up mewl for food at 4am, for i hate cucumber pls dont throw it at me meow and walk away. Poop in the plant pot under the bed. Bathe private parts with tongue then lick owner's face. Sit on human they not getting up ever bury the poop bury it deep purr for no reason yet attack feet.
+
+  Pee in the shoe growl at dogs in my sleep. Chase imaginary bugs destroy house in 5 seconds for fall over dead (not really but gets sypathy) get away from me stupid dog cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters' slippers. Swipe at owner's legs. Eat grass, throw it back up sitting in a box so under the bed, but eat all the power cords. Attempt to leap between furniture but woefully miscalibrate and bellyflop onto the floor; what's your problem? i meant to do that now i shall wash myself intently flex claws on the human's belly and purr like a lawnmower so proudly present butt to human so roll over and sun my belly. Time to go zooooom what the heck just happened, something feels fishy, claw your carpet in places everyone can see - why hide my amazing artistic clawing skills?. Sees bird in air, breaks into cage and attacks creature pet me pet me pet me pet me, bite, scratch, why are you petting me yet chirp at birds but chew foot, yet be a nyan cat, feel great about it, be annoying 24/7 poop rainbows in litter box all day meow all night eat owner's food. Mmmmmmmmmeeeeeeeeooooooooowwwwwwww nyaa nyaa put butt in owner's face yet loves cheeseburgers eat and than sleep on your face and have secret plans. Woops poop hanging from butt must get rid run run around house drag poop on floor maybe it comes off woops left brown marks on floor human slave clean lick butt now sleep on keyboard, tickle my belly at your own peril i will pester for food when you're in the kitchen even if it's salad or nyan fluffness ahh cucumber! why can't i catch that stupid red dot pretend you want to go out but then don't.
+  `;
+
+  return (
+    <section className="homepage">
+      <h2>Welcome to our site!</h2>
+      <p>{text}</p>
+    </section>
+  );
+};
+
+export default Home;
